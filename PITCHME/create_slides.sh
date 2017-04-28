8325a429c3b656206e02ffd82181b2c4295832af
vivekchi <vivekchi@users.noreply.github.com>
2017-04-29 00:47:04 +0530

    1

--- PITCHME/create_slides.sh
+++ PITCHME/create_slides.sh
@@ -1,8 +1,8 @@
-for md_file in $(ls ../../hugo/content/*md)
+for md_file in $(ls ../../hugo_content/content/*md)
 do
 	file_name=$(basename $md_file | sed 's/.md$//g')
 	echo "s/TEMPLATE_FILE_PLACEHOLDER/$file_name/g"
 	sed "s/TEMPLATE_FILE_PLACEHOLDER/$file_name/g" TEMPLATE_FILE.html > $file_name.html
 done
 
-cp  ../../content/*md .
+cp  ../../hugo_content/content/*md .
